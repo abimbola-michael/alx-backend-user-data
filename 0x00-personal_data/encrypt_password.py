54d291013bfaee7323c6fe1975947e92e986024e
Abimbola Michael <abimbolamichael100.com>
2024-02-08 13:11:37 +0100

    Personal Data tasks

---
+++ 0x00-personal_data/encrypt_password.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""Encrypting passwords"""
+import bcrypt
+
+
+def hash_password(password: str) -> str:
+    """
+    function that expects one string argument name
+    password and returns a salted, hashed password,
+    which is a byte string.
+    """
+    return bcrypt.hashpw(password.encode(), bcrypt.gensalt())
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """
+    function that uses bcrypt to validate that the provided
+    password matches the hashed password.
+    """
+    return bcrypt.checkpw(password.encode(), hashed_password)
---
+++ 0x00-personal_data/filtered_logger.py
@@ -0,0 +1,102 @@
+#!/usr/bin/env python3
+"""
+Filtering module
+"""
+
+import logging
+import re
+from typing import List
+from os import environ
+import mysql.connector
+
+
+PII_FIELDS = ('name', 'email', 'phone', 'ssn', 'password')
+
+
+def fliter_datum(
+        fields: List[str],
+        redaction: str,
+        message: str,
+        separator: str
+        ) -> str:
+    """
+    Returns the log message obfuscated
+    """
+    for field in fields:
+        message = re.sub(f'{field}=.*?{separator}',
+                         f'{field}={redaction}{separator}', message)
+    return message
+
+
+def get_logger() -> logging.Logger:
+    """
+    Returns a logging object
+    """
+    logger = logging.getLogger('user_data')
+    logger.setLevel(logging.INFO)
+    logger.propagate = False
+    stream = logging.StreamHandler()
+    formatter = RedactingFormatter(list(PII_FIELDS))
+    stream.setFormatter(formatter)
+    logger.addHandler(stream)
+    return logger
+
+
+def get_db() -> mysql.connector.connection.MySQLConnection:
+    """
+    Returns a connector to a database
+    """
+    username = environ.get('PERSONAL_DATA_DB_USERNAME', 'root')
+    password = environ.get('PERSONAL_DATA_DB_PASSWORD', '')
+    host = environ.get('PERSONAL_DATA_DB_HOST', 'localhost')
+    db_name = environ.get('PERSONAL_DATA_DB_NAME')
+    return mysql.connector.connect(
+        user=username,
+        password=password,
+        host=host,
+        database=db_name
+    )
+
+
+def main():
+    """
+    Main function
+    """
+    db = get_db()
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM users")
+    field_names = [i[0] for i in cursor.description]
+
+    logger = get_logger()
+    for row in cursor:
+        message = ''.join(f'{field}={str(value)}; ' for field,
+                          value in zip(field_names, row))
+        logger.info(message.strip())
+    cursor.close()
+    db.close()
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        """ Constructor method"""
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        """
+         filter values in incoming log records using filter_datum
+        """
+        record.msg = filter_datum(
+            self.fields, self.REDACTION, record.msg, self.SEPARATOR)
+        return super(RedactingFormatter, self).format(record)
+
+
+if __name__ == = "__main__":
+    main()
