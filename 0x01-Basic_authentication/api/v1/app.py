104a9397f9c3ea9ea8ccdd3371a712c98319a7cc
Abimbola Michael <abimbolamichael100.com>
2024-02-13 18:14:44 +0100

    Updae

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -8,10 +8,12 @@ from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
 import os
 
+
 app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 auth = None
+
 
 AUTH_TYPE = getenv("AUTH_TYPE")
 if AUTH_TYPE == "auth":
