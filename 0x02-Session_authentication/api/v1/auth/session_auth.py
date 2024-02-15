dd9223c8541a004595fc42c9ae19da6f9cea3e7b
Abimbola Michael <abimbolamichael100.com>
2024-02-15 15:45:41 +0100

    Pycode correction

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -29,7 +29,7 @@ class SessionAuth(Auth):
         if session_id is None or type(session_id) is not str:
             return None
         return self.user_id_by_session_id.get(session_id)
-    
+
     def current_user(self, request=None):
         """
         returns a User instance based on a cookie value
