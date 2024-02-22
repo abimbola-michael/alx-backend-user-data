7d4be2c8f02b1892edb98a20ef74e71f51577ce2
Abimbola Michael <abimbolamichael100.com>
2024-02-22 13:41:51 +0100

    Correction

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -53,6 +53,8 @@ def logout() -> str:
     Endpoint to logout a user
     """
     session_id = request.cookies.get("session_id")
+    if not session_id:
+        abort(403)
     user = AUTH.get_user_from_session_id(session_id)
     if not user:
         abort(403)
