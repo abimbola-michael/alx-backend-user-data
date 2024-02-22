6b6dc523b4074bce227b5411fe1c3521456b0cee
Abimbola Michael <abimbolamichael100.com>
2024-02-22 14:14:49 +0100

    Correction

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -97,14 +97,13 @@ def update_password() -> str:
     email = request.form.get("email")
     reset_token = request.form.get("reset_token")
     new_password = request.form.get("new_password")
-    if not reset_token or not new_password:
-        abort(403)
     try:
         AUTH.update_password(reset_token, new_password)
         return jsonify({"email": email, "message": "Password updated"}), 200
-    except Exception:
+    except ValueError:
         abort(403)
 
 
 if __name__ == "__main__":
+    """ Main Function"""
     app.run(host="0.0.0.0", port=5000)
